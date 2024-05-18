da7f6d033ea8c771951f9ee063e32caf8980ee3b
Chad Harper <chadharper811@gmail.com>
2024-05-18 15:44:10 -0500

    added setPlayerCard function

--- script.js
+++ script.js
@@ -183,8 +183,14 @@ headCoach.textContent = coachName;
 
 const setPlayerCards = ( arr = players ) => {
     playerCards.innerHTML += arr.map(({ name, position, number, isCaptain, nickname }) => {
-        `<div class="player-card">
+        `
+        <div class="player-card">
             <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
-        </div>`
-    });
+            <p>Position: ${position}</p>
+            <p>Number: ${number}</p>
+            <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
+        </div>
+        `;
+    }
+  ).join("");
 };
